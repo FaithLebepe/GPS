35fc2a3af581deff754e9eb686ce6d45471e86df
Faith <139481775+FaithLebepe@users.noreply.github.com>
2024-12-25 16:34:25 +0200

    done with this

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,141 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild  } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { GoogleMap, MapType } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
+
+  @ViewChild('map')
+  mapRef!: ElementRef<HTMLElement>;
+  newMap!: GoogleMap;
+  center: any = {
+    lat: -26.686746,
+    lng: 27.093026,
+  }
+  markerId: string = '';
+  
+  ngAfterViewInit(){
+    this.createMap();
+  }
+
   constructor() {}
+
+  async createMap() {
+    try {
+      this.newMap = await GoogleMap.create({
+        id: 'capacitor-google-maps',
+        element: this.mapRef.nativeElement,
+        apiKey: environment.google_maps_api_key,
+        config: {
+          center: this.center,
+          zoom: 13,
+        },
+      });
+
+      // move the app programmacally
+      await this.newMap.setCamera({
+        coordinate: {
+          lat: this.center.lat,
+          lng: this.center.lng,
+        },
+        animate: true
+      })
+
+      // enable Clustering
+      // await this.newMap.enableClustering();
+
+      // Enable traffic layer
+      await this.newMap.enableTrafficLayer(true);
+
+      await this.newMap.enableCurrentLocation(true);
+
+      // await this.newMap.setPadding({
+      //   top: 5,
+      //   left: 5,
+      //   right: 5,
+      //   bottom: 5,
+      // });
+      
+    //await this.newMap.setMapType(MapType.Hybrid);
+
+     this.addMarkers(this.center.lat, this.center.lng);
+     this.addListeners()
+    } catch(e){
+      console.log(e);
+    }
+  }
+
+  async addMarkers(lat: any, lng: any) {
+    // add a marker to the map
+    if (this.markerId) this.removeMarker();
+    await this.newMap.addMarkers([
+      {
+        coordinate: {
+          lat: lat,
+          lng: lng,
+        },
+        //title,
+        draggable: true
+      },
+
+      {
+        coordinate: {
+          lat: -26.68674,
+          lng: 27.09302,
+        },
+        //title,
+        draggable: true
+      },
+    ]);
+  }
+
+  async addMarker(lat: any, lng: any) {
+    // add a marker to the map
+    if (this.markerId) this.removeMarker();
+    this.markerId = await this.newMap.addMarker({
+      coordinate: {
+        lat: lat,
+        lng: lng,
+      },
+      //title,
+      draggable: true
+    });
+  }
+
+  async removeMarker(id?: any) {
+    await this.newMap.removeMarker(id ? id: this.markerId);
+  }
+
+  async addListeners() {
+    // handle marker click
+    await this.newMap.setOnMapClickListener((event) => {
+      console.log('setOnMapClickListener', event);
+    });
+
+    // await this.newMap.setOnCameraMoveStartedListener((event) => {
+    //   console.log(event);
+    // });
+
+    await this.newMap.setOnMapClickListener((event) => {
+      console.log('setOnMapClickListener', event);
+      this.addMarker(event.latitude, event.longitude);
+    });
+
+    await this.newMap.setOnMyLocationButtonClickListener((event) => {
+      console.log('setOnMyLocationButtonClickListener', event);
+      //this.addMarker(event.latitude, event.longitude);
+    });
+  
+    await this.newMap.setOnMyLocationClickListener((event) => {
+      console.log('setOnMyLocationClickListener', event);
+      this.addMarker(event.latitude, event.longitude);
+    });
+  }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  google_maps_api_key: 'AIzaSyC4sfsJF1t5w1oGq-ThALiu-H8FzmNLYac'
 };
 
 /*
